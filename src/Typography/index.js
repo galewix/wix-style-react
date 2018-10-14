366264a233a79d71d0de3c4943443cb70a11ac41
Erez Makavy <erez@wix.com>
2018-10-14 17:37:27 +0300

    Rename Typography_V5 to Typography_new

--- src/Typography/index.js
+++ src/Typography/index.js
@@ -12,7 +12,6 @@ const typographyProxy = new Proxy(typography, {
         `Typography class ${prop} is deprecated. Please use new classes described at https://wix-wix-style-react.surge.sh/?selectedKind=Styling&selectedStory=1.2%20Typography%20Classes`,
         prop
         );
-
     }
     return target[prop];
   }
