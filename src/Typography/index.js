3e3e9b0568eb38a8db1beacd72222421b48debb6
Erez Makavy <erez@wix.com>
2018-10-14 13:47:13 +0300

    Fix Typography to use new deprecationLog with key

--- src/Typography/index.js
+++ src/Typography/index.js
@@ -1,16 +1,17 @@
 import typography from './Typography.scss';
-import {ClassDeprecationLogger} from './ClassDeprecationLogger';
+import deprecationLog from '../utils/deprecationLog';
 
 export * from './Utils';
 
 const deprecatedRegExp = new RegExp('^([ht][1-6]_[1-6]|t[1-6])$');
 
-const deprecationLogger = new ClassDeprecationLogger();
-
 const typographyProxy = new Proxy(typography, {
   get(target, prop) {
     if (deprecatedRegExp.test(prop)) {
-      deprecationLogger.log(prop, `Typography class ${prop} is deprecated. Please use new classes described at https://wix-wix-style-react.surge.sh/?selectedKind=Styling&selectedStory=1.2%20Typography%20Classes`);
+      deprecationLog(
+        `Typography class ${prop} is deprecated. Please use new classes described at https://wix-wix-style-react.surge.sh/?selectedKind=Styling&selectedStory=1.2%20Typography%20Classes`,
+        prop
+        );
 
     }
     return target[prop];
