2765186e572269b5bc68b5c408c53340ca7d1646
Erez Makavy <erez@wix.com>
2018-10-14 13:44:29 +0300

    Remove ClassDeprecationLogger, add key to deprecationLog.

--- src/utils/deprecationLog.spec.js
+++ src/utils/deprecationLog.spec.js
@@ -14,4 +14,14 @@ describe('deprecationLog', () => {
     deprecationLog('Some message');
     expect(global.console.warn).toBeCalledWith('Wix-Style-React: [WARNING] Some message');
   });
+
+  it('should log only once per class', () => {
+    deprecationLog('message', 'keyA');
+    expect(global.console.warn.mock.calls.length).toEqual(1);
+    deprecationLog('message', 'keyA');
+    expect(global.console.warn.mock.calls.length).toEqual(1);
+    deprecationLog('message', 'keyB');
+    expect(global.console.warn.mock.calls.length).toEqual(2);
+  });
+
 });
